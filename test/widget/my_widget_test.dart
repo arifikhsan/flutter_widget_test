bf32d71e1964e36e2bd703550652f7f323fd2e75
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-05-26 22:58:23 +0700

    find a widget

--- test/widget/my_widget_test.dart
+++ test/widget/my_widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_widget_test/widget/my_widget.dart';
 
@@ -11,5 +12,35 @@ void main() {
     expect(titleFinder, findsOneWidget);
     expect(messageFinder, findsWidgets);
     expect(messageFinder, findsNWidgets(1));
+  });
+
+  group('find widgets', () {
+    testWidgets('finds a Text widget', (WidgetTester tester) async {
+      await tester.pumpWidget(MyWidget(title: 'Title', message: 'Message'));
+
+      expect(find.text('Title'), findsOneWidget);
+    });
+
+    testWidgets('finds a widget using a Key', (WidgetTester tester) async {
+      final testKey = Key('Key');
+
+      await tester.pumpWidget(MyWidget(
+        key: testKey,
+        title: 'Title',
+        message: 'Message',
+      ));
+
+      expect(find.byKey(testKey), findsOneWidget);
+    });
+
+    testWidgets('finds a specific instance', (WidgetTester tester) async {
+      final childWidget = Padding(padding: EdgeInsets.zero);
+
+      // Provide the childWidget to the Container.
+      await tester.pumpWidget(Container(child: childWidget));
+
+      // Search for the childWidget in the tree and verify it exists.
+      expect(find.byWidget(childWidget), findsOneWidget);
+    });
   });
 }
